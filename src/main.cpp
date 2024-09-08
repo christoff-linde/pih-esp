29b8f339c2ad858c4c9181367a8b95c4d101d86e
christoff-linde <christoff.linde@gmail.com>
2024-09-08 21:03:11 +0200

    refactor(main.cpp): replace old HTTP code with pubsub mqtt logic
    the current logic only caters for sending basic text with mqtt. the
    previous logic to send temperature data should be added next to ensure
    parity with previous features pre-mqtt rework.

--- src/main.cpp
+++ src/main.cpp
@@ -1,162 +1,131 @@
-#include "WiFi.h"
-#include <HTTPClient.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
+/*
+ Basic ESP8266 MQTT example
+ This sketch demonstrates the capabilities of the pubsub library in combination
+ with the ESP8266 board/library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+*/
 
-#define DHTPIN D2
-#define DHTTYPE DHT22
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
-uint32_t delayMS;
+// Update these with values suitable for your network.
 
-const char *ssid = "";
-const char *password = "";
+// TODO: change following values
+// TODO: find way to set "env" variables
+const char* ssid = "...";
+const char* password = "...";
+const char* mqtt_server = "192.168.0.105";
 
-HTTPClient http;
-String serverName = "http://192.168.0.101:8000/api/client/v1/update-sensor";
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
 
-void initWifi()
-{
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
     Serial.print(".");
-    delay(1000);
   }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-void networkScan()
-{
-  Serial.println("Scan start");
-
-  // WiFi.scanNetworks will return the number of networks found
-  int numberOfNetworks = WiFi.scanNetworks();
-
-  if (numberOfNetworks == 0)
-  {
-    Serial.println("No networks found");
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
   }
-  else
-  {
-    Serial.print(numberOfNetworks);
-    Serial.println(" networks found");
-    for (int index = 0; index < numberOfNetworks; ++index)
-    {
-      // Print SSID and RSSI for each network found
-      Serial.print(index + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(index));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(index));
-      Serial.print(")");
-      Serial.println(WiFi.encryptionType(index) == WIFI_AUTH_OPEN ? " " : "*");
-      delay(10);
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("pih", "hello world");
+      // ... and resubscribe
+      client.subscribe("pih");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
     }
   }
-  Serial.println("Scan done");
-  Serial.println("");
 }
 
-void initSensors()
-{
-  dht.begin();
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("%"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("%"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
-}
-
-void setup()
-{
-  delay(100);
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
-  Serial.println("Starting setup");
-
-  // Set WiFi to station mode and disconnect from an AP if it was previously connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  networkScan();
-  initWifi();
-  initSensors();
-  Serial.println("Setup done");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 }
 
-void loop()
-{
-  delay(delayMS);
+void loop() {
 
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  float temperature = event.temperature;
-  if (isnan(temperature))
-  {
-    Serial.println(F("Error reading temperature!"));
+  if (!client.connected()) {
+    reconnect();
   }
-  else
-  {
-    Serial.print(F("Temperature: "));
-    Serial.print(temperature);
-    Serial.println(F("°C"));
-  }
+  client.loop();
 
-  dht.humidity().getEvent(&event);
-  float humidity = event.relative_humidity;
-  if (isnan(humidity))
-  {
-    Serial.println(F("Error reading humidity!"));
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("pih", msg);
   }
-  else
-  {
-    Serial.print(F("Humidity: "));
-    Serial.print(humidity);
-    Serial.println(F("%"));
-  }
-
-  String serverPath = serverName + "?sensor_id=DHT22_01&temperature=" + String(temperature) + "&humidity=" + String(humidity);
-  http.begin(serverPath.c_str());
-
-  int httpResponseCode = http.GET();
 }
