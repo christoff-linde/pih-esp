c9e9ce985b7c0c5c08ff03cf602eaeb50a172f4c
christoff-linde <christoff.linde@gmail.com>
2024-09-08 21:33:15 +0200

    feat(main.cpp): add DHT sensor data recording
    the sensor readings are now published to their relevant topics in
    RabbitMQ (still requires some refinement)

--- src/main.cpp
+++ src/main.cpp
@@ -20,81 +20,175 @@
 
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include "Arduino.h"
+
+#define DHTPIN D2
+#define DHTTYPE DHT22
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
 
 // Update these with values suitable for your network.
 
 // TODO: change following values
 // TODO: find way to set "env" variables
-const char* ssid = "...";
-const char* password = "...";
-const char* mqtt_server = "192.168.0.105";
+const char *ssid = "";
+const char *password = "";
+const char *mqtt_server = "192.168.0.105";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
+#define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
+void initWifi()
+{
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
+    delay(1000);
   }
-
-  randomSeed(micros());
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void networkScan()
+{
+  Serial.println("Scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int numberOfNetworks = WiFi.scanNetworks();
+
+  if (numberOfNetworks == 0)
+  {
+    Serial.println("No networks found");
+  }
+  else
+  {
+    Serial.print(numberOfNetworks);
+    Serial.println(" networks found");
+    for (int index = 0; index < numberOfNetworks; ++index)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(index + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(index));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(index));
+      Serial.print(")");
+      Serial.println(WiFi.encryptionType(index) == WIFI_AUTH_OPEN ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("Scan done");
+  Serial.println("");
+}
+
+void initSensors()
+{
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 
   // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  if ((char)payload[0] == '1')
+  {
+    digitalWrite(BUILTIN_LED, LOW); // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
-
+  else
+  {
+    digitalWrite(BUILTIN_LED, HIGH); // Turn the LED off by making the voltage HIGH
+  }
 }
 
-void reconnect() {
+void initMqtt()
+{
+  Serial.println("Init MQTT");
+  // Set MQTT properties
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ESP32Client-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str()))
+    {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("pih", "hello world");
+      // client.publish("pih", "hello world");
       // ... and resubscribe
-      client.subscribe("pih");
-    } else {
+      // client.subscribe("pih/temperature");
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -104,28 +198,70 @@ void reconnect() {
   }
 }
 
-void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+void setup()
+{
+  pinMode(BUILTIN_LED, OUTPUT); // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  networkScan();
+  initWifi();
+  initSensors();
+  initMqtt();
+
+  Serial.println("Setup done");
 }
 
-void loop() {
-
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 
   unsigned long now = millis();
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > delayMS)
+  {
     lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("pih", msg);
+    // ++value;
+    // snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    // Serial.print("Publish message: ");
+    // Serial.println(msg);
+    // client.publish("pih", msg);
+    // delay(delayMS);
+
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    float temperature = event.temperature;
+    if (isnan(temperature))
+    {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else
+    {
+      Serial.print(F("Temperature: \t"));
+      Serial.print(temperature);
+      Serial.println(F("°C"));
+      client.publish("pih/temperature", String(temperature).c_str());
+    }
+
+    dht.humidity().getEvent(&event);
+    float humidity = event.relative_humidity;
+    if (isnan(humidity))
+    {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else
+    {
+      Serial.print(F("Humidity: \t"));
+      Serial.print(humidity);
+      Serial.println(F("%"));
+      client.publish("pih/humidity", String(humidity).c_str());
+    }
   }
 }
